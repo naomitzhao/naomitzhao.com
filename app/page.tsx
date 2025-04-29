fee6ae372f89b1662c5ccb4a2b153654381146b9
naomi zhao <naomitan.zhao@gmail.com>
2025-04-29 15:03:30 -0700

    fix: Escape apostrophe in main text

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <div className={styles.content}>
-        <h1>hi, i'm naomi!</h1>
+        <h1>hi, i&apos;m naomi!</h1>
         <p>welcome to my personal site.</p>
         <p>currently a work in progres... maybe check back later?</p>
       </div>
