6fb7eda90714e936d4e556a2dbafec7b8ff6d260
Naomi Zhao <naomitan.zhao@gmail.com>
2025-05-01 15:16:10 -0700

    feat: Update content

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,23 @@ export default function Home() {
           <h1>hi, i&apos;m naomi!</h1>
           <div className={styles.subTexts}>
             <p>i&apos;m someone who&apos;s passionate about full-stack development, user experience, and learning as much as i can.</p>
-            <p>check out my github to see some of my work!</p>
+            <p>check out my github to see more of my work!</p>
           </div>
+          <h2>some things i&apos;ve worked on...</h2>
+          <ul>
+            <li>
+              <p>made <a href="https://naomitzhao.github.io/top-projects/">13 javascript mini projects</a> from <a href="https://www.theodinproject.com/">the odin project</a> in one summer</p>
+            </li>
+            <li>
+              <p>developed homepage and various components for client project: <a href="https://ucdbestbuddies.org/">ucd best buddies</a></p>
+            </li>
+            <li>
+              <p>led development for client project: <a href="https://www.joanviterimemorialclinic.com/">joan viteri memorial clinic</a></p>
+            </li>
+            <li>
+              <p>currently leading development for client project: uc davis aggie fiction...</p>
+            </li>
+          </ul>
         </div>
         <div className={styles.socialsIcons}>
           <a href="https://www.linkedin.com/in/naomitzhao/"><FaLinkedin className={styles.socialIcon}/></a>
