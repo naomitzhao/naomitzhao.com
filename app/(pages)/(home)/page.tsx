2dadee09a0ab14170ac3ebc30aab0b694673dca6
Naomi Zhao <naomitan.zhao@gmail.com>
2026-03-27 19:12:27 -0700

    feat: Add home hero and home about section

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,48 +1,35 @@
+import Image from "next/image";
 import styles from "./page.module.scss";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <div className={styles.content}>
-        <div className={styles.textContent}>
-          <div className={styles.textSection}>
-            <h1>hi, i{'\''}m naomi!</h1>
-            <p>i{'\''}m passionate about making beautiful and functional software {'<'}3</p>
-          </div>
-          <div className={styles.textSection}>
-            <h2>some things i{'\''}ve worked on...</h2>
-            <ul>
-              <li>
-                <p>made <a href="https://naomitzhao.github.io/top-projects/">13 javascript mini projects</a> from <a href="https://www.theodinproject.com/">the odin project</a> in one summer</p>
-              </li>
-              <li>
-                <p>developed homepage and various components for client project: <a href="https://ucdbestbuddies.org/">ucd best buddies</a></p>
-              </li>
-              <li>
-                <p>tech lead for client project: <a href="https://www.joanviterimemorialclinic.com/">joan viteri memorial clinic</a></p>
-              </li>
-            </ul>
-          </div>
-          <div className={styles.textSection}>
-            <h2>what i&apos;m doing now</h2>
-          <ul>
-            <li>
-              <p>web software engineering internship at <a href="https://robinhood.com/">robinhood</a></p>
-            </li>
-            <li>
-              <p>web app client project for uc davis aggie fiction</p>
-            </li>
-            <li>
-              <p>research at collaborative and social computing (csc) lab at uc davis</p>
-            </li>
-            </ul>
-          </div>
-          <p>check out my github to see more of my work!</p>
+      <div className={styles.hero}>
+        <div className={styles.stickerWrapper}>
+          <Image src="/naomi_sticker.png" fill alt="Sticker of Naomi Zhao"/>
         </div>
-        <div className={styles.socialsIcons}>
-          <a href="https://www.linkedin.com/in/naomitzhao/"><FaLinkedin className={styles.socialIcon}/></a>
-          <a href="https://github.com/naomitzhao"><FaGithub className={styles.socialIcon}/></a>
+        <div className={styles.speech}>
+          <Image src="/speech_bubble.svg" fill alt="Speech bubble"/>
+          <div className={styles.speechText}>
+            <h2>HEY! I&apos;m Naomi 🙀</h2>
+            <p>Welcome to my corner of the internet!</p>
+            <p>I&apos;m a software engineer who loves web dev, devouring yummy food, and living my best life (yippee!)</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.homeAbout}>
+        <div className={styles.naomiIconWrapper}>
+          <Image src="/naomi_linkedin.jpeg" alt="Naomi headshot photo" fill/>
+        </div>
+        <div className={styles.homeAboutText}>
+          <h2>I am currently up to...</h2>
+          <ul>
+            <li>studying computer science and engineering (BS) and economics (BA) at UC Davis</li>
+            <li>leading #include at Davis</li>
+            <li>living my best life :D</li>
+          </ul>
+          <p>To see my full experiences, check out the <Link href="/about">about page</Link>!</p>
         </div>
       </div>
     </div>
